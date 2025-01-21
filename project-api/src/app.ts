4cac6a2778ef58b272e860b41bf55ae4e0936179
Victormrf <vmrf2000@hotmail.com>
2025-01-21 04:23:13 -0300

    Added database

--- project-api/src/app.ts
+++ project-api/src/app.ts
@@ -1,4 +1,15 @@
 import express from "express";
+import * as mysql from "mysql2/promise";
+import bcrypt from "bcrypt";
+
+function createConnection() {
+  return mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "tickets",
+  });
+}
 
 const app = express();
 
@@ -14,12 +25,54 @@ app.post("/auth/login", (req, res) => {
   res.send();
 });
 
-app.post("/partners", (req, res) => {
+app.post("/partners", async (req, res) => {
   const { name, email, password, company_name } = req.body;
+
+  const connection = await createConnection();
+
+  try {
+    const createdAt = new Date();
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const [userResult] = await connection.execute<mysql.ResultSetHeader>(
+      "INSERT INTO users (name, email, password, created_at)",
+      [name, email, hashedPassword, createdAt]
+    );
+    const userId = userResult.insertId;
+    const [partnerResult] = await connection.execute<mysql.ResultSetHeader>(
+      "INSERT INTO partners (user_id, company_namer, created_at)",
+      [userId, company_name, createdAt]
+    );
+
+    res
+      .status(201)
+      .json({ id: partnerResult.insertId, userId, company_name, createdAt });
+  } finally {
+    connection.end();
+  }
 });
 
 app.post("/customers", (req, res) => {
-  const { name, email, password, company_name } = req.body;
+  const { name, email, password, address, phone } = req.body;
+});
+
+app.post("/partners/events", (req, res) => {
+  const { name, description, date, location } = req.body;
+});
+
+app.get("/partners/events", (req, res) => {});
+
+app.get("/partners/events/:eventId", (req, res) => {
+  const { eventId } = req.params;
+  console.log(eventId);
+  res.send();
+});
+
+app.get("/events", (req, res) => {});
+
+app.get("/events/:eventId", (req, res) => {
+  const { eventId } = req.params;
+  console.log(eventId);
+  res.send();
 });
 
 app.listen(3000, () => {
