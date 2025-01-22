6e24751e277328dbfa50e710ec91e3d28206a2b9
Victormrf <vmrf2000@hotmail.com>
2025-01-22 04:36:36 -0300

    Implemented jwt athentication on routes

--- project-api/src/app.ts
+++ project-api/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as mysql from "mysql2/promise";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 function createConnection() {
   return mysql.createConnection({
@@ -8,6 +9,7 @@ function createConnection() {
     user: "root",
     password: "root",
     database: "tickets",
+    port: 33060,
   });
 }
 
@@ -15,17 +17,78 @@ const app = express();
 
 app.use(express.json());
 
+const unprotectedRoutes = [
+  { method: "POST", path: "/auth/login" },
+  { method: "POST", path: "/customers/register" },
+  { method: "POST", path: "/partners/register" },
+  { method: "GET", path: "/events" },
+];
+
+app.use(async (req, res, next) => {
+  const isUnprotectedRoute = unprotectedRoutes.some(
+    (route) => route.method == req.method && req.path.startsWith(route.path)
+  );
+
+  if (isUnprotectedRoute) {
+    return next();
+  }
+
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ message: "No token provided" });
+    return;
+  }
+  try {
+    const payload = jwt.verify(token, "123456") as {
+      id: number;
+      email: string;
+    };
+    const connection = await createConnection();
+    const [rows] = await connection.execute<mysql.RowDataPacket[]>(
+      "SELECT * FROM users WHERE email = ?",
+      [payload.id]
+    );
+    const user = rows.length ? rows[0] : null;
+    if (!user) {
+      res.status(401).json({ message: "Failed to authenticate token" });
+      return;
+    }
+    req.user = user as { id: number; email: string };
+    next();
+  } catch (e) {
+    res.status(401).json({ message: "Failed to authenticate token" });
+  }
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello world!" });
 });
 
-app.post("/auth/login", (req, res) => {
+app.post("/auth/login", async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
+  const connection = await createConnection();
+  try {
+    const [rows] = await connection.execute<mysql.RowDataPacket[]>(
+      "SELECT * FROM users WHERE email = ?",
+      [email]
+    );
+    const user = rows.length ? rows[0] : null;
+    if (user && bcrypt.compareSync(password, user.password)) {
+      // generar o jwt
+      const token = jwt.sign({ id: user.id, email: user.email }, "123456", {
+        expiresIn: "1h",
+      });
+      res.json({ token });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } finally {
+    await connection.end();
+  }
   res.send();
 });
 
-app.post("/partners", async (req, res) => {
+app.post("/partners/register", async (req, res) => {
   const { name, email, password, company_name } = req.body;
 
   const connection = await createConnection();
@@ -34,25 +97,56 @@ app.post("/partners", async (req, res) => {
     const createdAt = new Date();
     const hashedPassword = bcrypt.hashSync(password, 10);
     const [userResult] = await connection.execute<mysql.ResultSetHeader>(
-      "INSERT INTO users (name, email, password, created_at)",
+      "INSERT INTO users (name, email, password, created_at) VALUES (?, ?, ?, ?)",
       [name, email, hashedPassword, createdAt]
     );
     const userId = userResult.insertId;
     const [partnerResult] = await connection.execute<mysql.ResultSetHeader>(
-      "INSERT INTO partners (user_id, company_namer, created_at)",
+      "INSERT INTO partners (user_id, company_name, created_at) VALUES (?, ?, ?)",
       [userId, company_name, createdAt]
     );
 
-    res
-      .status(201)
-      .json({ id: partnerResult.insertId, userId, company_name, createdAt });
+    res.status(201).json({
+      id: partnerResult.insertId,
+      name,
+      userId,
+      company_name,
+      createdAt,
+    });
   } finally {
     connection.end();
   }
 });
 
-app.post("/customers", (req, res) => {
+app.post("/customers/register", async (req, res) => {
   const { name, email, password, address, phone } = req.body;
+
+  const connection = await createConnection();
+
+  try {
+    const createdAt = new Date();
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const [userResult] = await connection.execute<mysql.ResultSetHeader>(
+      "INSERT INTO users (name, email, password, created_at) VALUES (?, ?, ?, ?)",
+      [name, email, hashedPassword, createdAt]
+    );
+    const userId = userResult.insertId;
+    const [partnerResult] = await connection.execute<mysql.ResultSetHeader>(
+      "INSERT INTO customers (user_id, address, phone, created_at) VALUES (?, ?, ?, ?)",
+      [userId, address, phone, createdAt]
+    );
+
+    res.status(201).json({
+      id: partnerResult.insertId,
+      name,
+      userId,
+      address,
+      phone,
+      createdAt,
+    });
+  } finally {
+    connection.end();
+  }
 });
 
 app.post("/partners/events", (req, res) => {
@@ -75,6 +169,13 @@ app.get("/events/:eventId", (req, res) => {
   res.send();
 });
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+  const connection = await createConnection();
+  await connection.execute("SET FOREIGN_KEY_CHECKS = 0");
+  await connection.execute("TRUNCATE TABLE events");
+  await connection.execute("TRUNCATE TABLE customers");
+  await connection.execute("TRUNCATE TABLE partners");
+  await connection.execute("TRUNCATE TABLE users");
+  await connection.execute("SET FOREIGN_KEY_CHECKS = 1");
   console.log("Server is running in http://localhost:3000");
 });
